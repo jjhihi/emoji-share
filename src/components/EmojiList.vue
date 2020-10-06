a357a018bb29c86a0fe69d34f49935f2ae60e7e4
차지원(Jiwon Cha)/DevOps팀/SKP <jiwon.cha@sk.com>
2020-10-06 21:39:25 +0900

    pagination, search bar, media query, side menu

--- src/components/EmojiList.vue
+++ src/components/EmojiList.vue
@@ -1,27 +1,74 @@
 <template>
-    <v-container fluid>
-      <v-row justify="end">
-      <v-btn-toggle mandatory @change="changeListType" v-model="listType">
-          <v-btn value="list">
+<v-card class="mx-auto test-width" >
+    <v-toolbar
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Emoji List</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+<v-text-field hide-details="auto" prepend-icon="mdi-magnify" :value="keyword" @change="setKeyword">
+</v-text-field>
+
+      <v-btn-toggle mandatory group @change="changeListType" v-model="listType">
+          <v-btn icon value="list">
             <v-icon>mdi-format-list-bulleted-square</v-icon>
           </v-btn>
-          <v-btn value="grid">
+          <v-btn icon value="grid">
             <v-icon>mdi-view-grid</v-icon>
           </v-btn>
         </v-btn-toggle>
-      </v-row>
+    </v-toolbar>
+<v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-container fluid>
       <v-row dense v-if="listType == 'grid'">
         <v-col
           v-for="(card, index) in cards"
           :key="index"
           :cols="6"
-        >
+          xs="6"
+          sm="4"
+          md="3"
+          lg="3"
+          xl="3">
           <v-card>
             <v-img
               :src="baseUrl+card['thumbnail-iamge']"
               class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px" >
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)">
               <v-card-title v-text="card.description"></v-card-title>
             </v-img>
 
@@ -82,14 +129,42 @@
         </v-list-item>
       </template>
     </v-list>
+<div class="text-center">
+    <v-pagination
+      v-model="page"
+      :length="15"
+      :total-visible="7"
+      circle
+      @input="setPage"
+    ></v-pagination>
+  </div>
+<v-fab-transition>
+        <v-btn
+            v-scroll="onScroll"
+            v-show="fab"
+            fab
+            fixed
+            bottom
+            right
+color="primary"
+            @click="toTop"
+          >
+        <v-icon>mdi-chevron-double-up</v-icon>
+          </v-btn>
+</v-fab-transition>
       
     </v-container>
+</v-card>
 </template>
 
 <script>
   export default {
     name: 'EmojiList',
     data: () => ({
+fab:false,
+      page: 1,
+      keyword: "",
+      drawer: false,
       cards: [], 
       show: [],
       listType: undefined,
@@ -97,8 +172,22 @@
       baseUrl: "/sample_data/",
     }),
     methods: {
+onScroll (e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset ||   e.target.scrollTop || 0
+      this.fab = top > 20
+    },
+    toTop () {
+      this.$vuetify.goTo(0)
+    },
+      setKeyword(keyword) {
+         console.log(keyword);
+      },
+      setPage( page ) {
+         this.page = page;
+      },
       callAppShare( index ) {
-        console.log(index, this.cards[index]);
+        //console.log(index, this.cards[index]);
         //window.Android.doShare( arg1, arg2, arg3 ); 
       },
       changeListType(key) {
@@ -143,7 +232,7 @@
        self.listType = "grid";
     },
     updated() {
-console.log("updated", this.show);
+//console.log("updated", this.show);
     }
   }
 </script>
